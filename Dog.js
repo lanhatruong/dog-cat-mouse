08a4db450fbe4f23c2347cd2457734268e6e687d
lanhatruong <lanhatruong@gmail.com>
2019-10-24 19:09:25 +0700

    SayHi menthod Dog

--- Dog.js
+++ Dog.js
@@ -1,6 +1,13 @@
-function Dog(){
+var chalk = require('chalk')
+
+function Dog(name){
+    this.name = name;
     this.stomach = [];
 }
 Dog.prototype.eat = function(cat){
     this.stomach.push(cat);
 };
+Dog.prototype.sayHi = function(){
+    console.log('Hi, IM Dog. My Name is ' + chalk.red(this.name));
+}
+module.exports = Dog;
